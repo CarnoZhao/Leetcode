86be53ae139ef3c2ba1983c1b6eccdb76318a440
CarnoZhao <273806108@qq.com>
2019-05-12 10:03:15 -0400

    realGIVEUP

--- CodeWar/Square_sums.py
+++ CodeWar/Square_sums.py
@@ -31,13 +31,14 @@ def function(n):
     # dic[0] = list(range(1, n + 1)
     start = 1
     path = [start]
-    path = extend_path(dic, path, start)
-    path = extend_path(dic, path[::-1], path[-1])
+    path = extend_path_full(dic, path, start)
+    path = extend_path_full(dic, path[::-1], path[-1])
     for num in dic:
         if num not in path:
             print(num, [x for x in dic[num]])
-    print(path)
-    print(len(path))
+    # print(str(path)[1:-1])
+    # print(len(path))
+    return path
 
 def bfs(n):
     squares = [x ** 2 for x in range(1, n + 1) if x ** 2 < 2 * n]
@@ -55,12 +56,40 @@ def bfs(n):
                     continue
                 temp.append(path + [new])
         paths = temp
-        print(len(paths))
-    print(paths)
+    # for path in paths:
+    #     print(', '.join(str(x) for x in path))
+    #     print()
+    return paths
+
+def dfs(n):
+    squares = [x ** 2 for x in range(1, n + 1) if x ** 2 < 2 * n]
+    dic = {}
+    for num in range(1, n + 1):
+        dic[num] = [x - num for x in squares if 0 < x - num <= n and x != 2 * num]
+        dic[num].append(0)
+    dic[0] = list(range(1, n + 1))
+    path = dfs_extend(dic, [0])
+    return path
+
+def dfs_extend(dic, path):
+    for nex in dic[path[-1]]:
+        if nex not in path:
+            ret = dfs_extend(dic, path + [nex])
+            if ret and len(ret) == n + 1:
+                return ret
+    return path in len(path) == n + 1 else None
 
 if __name__ == '__main__':
-    bfs(40)
-    # for n in range(40, 41):
-    #     ret = function(n)
-    #     if ret:
-    #         print(n, ret)
+    n = 50
+    print(dfs(n))
+    # paths = bfs(n)
+    # path = function(n)
+    # maxcor = 0
+    # maxp = []
+    # for p in paths:
+    #     correct = max(sum(x == y for x, y in zip(p, path[i:] + path[:i])) for i in range(len(path)))
+    #     if correct > maxcor:
+    #         maxcor = correct
+    #         maxp = p
+    # print(str(maxp)[1:-1])
+    # print(str(path)[1:-1])
