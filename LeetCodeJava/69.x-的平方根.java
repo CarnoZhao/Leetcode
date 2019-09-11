eca5296aa719cb8352348f5b22b5f8c8c3e2ee8e
CarnoZhao <273806108@qq.com>
2019-09-11 09:06:56 +0800

    2019.9.11

--- LeetCodeJava/69.x-的平方根.java
+++ LeetCodeJava/69.x-的平方根.java
@@ -5,8 +5,15 @@
  */
 class Solution {
     public int mySqrt(int x) {
-        int l = 1;
-        int r = x;
+        if (x == 0) {
+            return 0;
+        } else if (x < 4) {
+            return 1;
+        } else if (x < 9) {
+            return 3;
+        }
+        int l = 0;
+        int r = x / 2;
         int m;
         while (r - l > 1) {
             m = (l + r) / 2;
@@ -15,7 +22,7 @@ class Solution {
             } else if (m * m > x) {
                 r = m;
             } else {
-                break;
+                return m;
             }
         }
         return l;
